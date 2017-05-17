aace81852f9fc7821ad8f16ed3d86eaffb5a87bd
palosa <pablodeep14@gmail.com>
2017-05-17 08:58:12 +0200

    FormularioPedidos

---
+++ formularioPedidos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="" action="formularioPedidos.php" method="post">
+      <p>Productos</p>
+      <select class="" name="productos" required>
+        <option value="">productos</option>
+      </select>
+      <select class="" name="productos" required>
+        <option value="">productos</option>
+      </select>
+      <select class="" name="productos" required>
+        <option value="">productos</option>
+      </select>
+      <select class="" name="productos" required>
+        <option value="">productos</option>
+      </select>
+      <p>Proveedor</p>
+      <select class="" name="proveedor" required>
+        <option value="">proveedor</option>
+      </select>
+      <select class="" name="proveedor" required>
+        <option value="">proveedor</option>
+      </select>
+      <select class="" name="proveedor" required>
+        <option value="">proveedor</option>
+      </select>
+      <select class="" name="proveedor" required>
+        <option value="">proveedor</option>
+      </select>
+      <p>Fecha</p>
+      <input type="date" name="fecha" value="" required><br><br>
+
+      <input type="submit" name="" value="Enviar Pedido">
+    </form>
+  </body>
+</html>
