f792d41d0de641c117a1a7dee389954584b66ee9
AndreuSan <andreusym@gmail.com>
2017-05-25 00:50:53 +0200

    Add files via upload

--- controlproductos.php
+++ controlproductos.php
@@ -1,164 +1,74 @@
+<?php
+//incluimos y creamos todos los objetos necesarios
+include 'seguridad/seguridad.php';
+include '/modelo/producto.php';
+$sesion=new Seguridad();
+$pedido= new Producto();
+//comprobamos si la sesion esta iniciada.
+  if (isset($_SESSION["usuario"])==false) {
+    //si no esta iniciada lo enviamos a login para que la inicie
+    header('Location: login.php');
+  }else {
+    //si esta iniciada seguimos con la pagina
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Menu</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
-    <style>
-* {
-  box-sizing: border-box;
-}
-.myInput {
-  background-image: url('/css/searchicon.png');
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  width: 100%;
-  font-size: 16px;
-  padding: 12px 20px 12px 40px;
-  border: 1px solid #ddd;
-  margin-bottom: 12px;
-}
-#myTable {
-  border-collapse: collapse;
-  width: 100%;
-  border: 1px solid #ddd;
-  font-size: 18px;
-}
-#myTable th, #myTable td {
-  text-align: left;
-  padding: 12px;
-}
-#myTable tr {
-  border-bottom: 1px solid #ddd;
-}
-#myTable tr.header, #myTable tr:hover {
-  background-color: #f1f1f1;
-}
-</style>
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <title>Pedidos</title>
+    <link rel="stylesheet" href="css/css.css">
   </head>
   <body>
-    <div class="container">
-        <ul class="nav nav-pills">
-          <li class="active"><a data-toggle="pill" href="#home">Pedido</a></li>
-          <li><a data-toggle="pill" href="#menu1">Pedidos Realizados</a></li>
-        </ul>
-        <div class="tab-content">
-
-          <div id="home" class="tab-pane fade in active">
-
-            <form  action="controlproductos.php" method="post">
-              <label for="">Producto</label><br>
-              <input type="text" name="producto" value=""><br>
-
-              <label for="">Proveedor</label><br>
-              <input type="text" name="proveedor" value=""><br>
-
-              <label for="">Fecha</label><br>
-              <input type="date" name="fecha" value="">
-
-              <input type="submit" name="" value="Enviar">
-            </form>
-            <?php
-            if (isset($_POST['producto']) && isset($_POST['proveedor']) && isset($_POST['fecha'])){
-              include '\modelo\productos.php';
-             $producto = new Productos();
-              //llamada a la funcion de insertar usuario en la db
-              $resultado=$producto->insertarproducto($_POST['producto'], $_POST['proveedor'], $_POST['fecha']);
-            }
-             ?>
-          </div>
-
-          <div id="menu1" class="tab-pane fade">
-            <h2>Pedidos Realizados</h2>
-
-              <input type="text" class="myInput" id="productoInput" onkeyup="filter()" placeholder="Busca por producto..." title="Type in a producto">
-              <input type="text" class="myInput" id="proveedorInput" onkeyup="filter()" placeholder="Busca por proveedor..." title="Type in a proveedor">
-              <input type="text" class="myInput" id="fechaInput" onkeyup="filter()" placeholder="Busca por fecha..." title="Type in a fecha">
-
-
-              <table id="productos">
-                <tr class="header">
-                  <th style="width:33.33%;">Producto</th>
-                  <th style="width:33.33%;">Proveedor</th>
-                  <th style="width:33.33%;">Fecha</th>
-                </tr>
-
-                <?php
-
-              include "modelo/productos.php";
-               $producto = new Productos();
-               $tabla=$producto->MostrarProductos();
-               foreach ($tabla as $fila) {
-               ?>
-               <tr>
-                 <td><?php echo $fila['producto']; ?></td>
-                 <td><?php echo $fila['proveedor']; ?></td>
-                 <td><?php echo $fila['fecha']; ?></td>
-               </tr>
-               <?php
-             }
-                ?>
-              </table>
-          </div>
-        </div>
-
-<script >
-  //Creamos las variables
-  var productoInput, proveedorInput, fechaInput, table, trs, tds, i;
-  var data;
-  var result;
-
-  function getData(){
-    table = document.querySelector("#producto");
-    trs = table.querySelectorAll("tr");
-    data = { rows: [] };
-    for(i = 1; i < trs.length; i++) {
-     tds = trs[i].querySelectorAll("td");
-  data.rows.push({
-         el: trs[i],
-            producto: tds[0].innerHTML.trim(),
-            proveedor: tds[1].innerHTML.trim(),
-            fecha: tds[2].innerHTML.trim(),
-        });
+    <!--Header de la pagina con banner y logo y el menu. -->
+    <header>
+      <div class="banner">
+      <img class="imagenlogo"src="images/logo.PNG" alt="">
+      <div class="enlaceBanner">
+        <ul>
+      <?php
+        if (isset($_SESSION['usuario'])) {
+          echo "<li><a href='logout.php'>Cerrar sesion</a></li>";
+        }else {
+          echo "<li><a href='login.php'>Iniciar sesion</a>
+          <a href='registro.php'>Registro</a></li>";
+        }
+       ?>
+     </ul>
+     </div>
+      </div>
+    </header>
+    <?php
+  private  function InsertarProducto($producto,$proveedor,$fecha){
+      $this->conexion->query("INSERT INTO producto (Producto, Proveedor, Fecha) VALUES ('".$producto."','".$proveedor."','".$fecha."')");
     }
-  }
-  function filter() {
-    productoInput = document.querySelector("#productoInput");
-    proveedorInput = document.querySelector("#proveedorInput");
-    fechaInput = document.querySelector("#fechaInput");
-    var containsproducto;
-    var containsproveedor;
-    var containsfecha;
-    result = { matches: [] }
-    for(i = 0; i < data.rows.length; i++) {
-  containsproducto = data.rows[i]
-        .producto.toLowerCase().includes(productoInput.value.toLowerCase());
-        containsproveedor = data.rows[i]
-        .proveedor.toLowerCase().includes(proveedorInput.value.toLowerCase());
-        containsfecha = data.rows[i]
-        .fecha.toLowerCase().includes(fechaInput.value.toLowerCase());
-      if(containsproducto && containsproveedor && containsfecha) {
-        data.rows[i].el.style.display = "";
-        // Store result for ajax request
-        result.matches.push( {
-         producto: data.rows[i].producto,
-            proveedor: data.rows[i].proveedor,
-            fecha: data.rows[i].fecha,
-        });
-      }else {
-       data.rows[i].el.style.display = "none";
-      }
+
+    if (isset($_POST)) {
+        $this->conexion->InsertarProducto($_POST['producto'],$_POST['proveedor'],$_POST['fecha']);
     }
-   // Print result for debug purposes
-    console.dir(result);
-  }
-  // Initialize data
-  getData();
-</script>
+     ?>
+        <form class="" action="controlproductos.php" method="post">
+          <label for="Producto">Producto</label>
+          <input type="text" name="producto" value="">
+
+          <label for="Proveedor">Proveedor</label>
+          <input type="text" name="proveedor" value="">
+
+          <label for="Fecha">Fecha</label>
+          <input type="date" name="fecha" value="">
+
+          <input type="submit" name="" value="Añadir Producto">
+        </form>
+  <!--Footer de la pagina -->
+  <footer>
+    <div class="text">
+    <p class="text-footer">Copyright (c) 2017 Copyright Holder All Rights Reserved.</p>
+    <a href="https://www.facebook.com/FoodyFood-1205351362909040/" target="nueva"><img class="footer-icon" src="images/facebook.png" alt=""></a>
+    <a href="https://www.instagram.com/foodyfood.florida/" target="nueva"><img class="footer-icon" src="images/insta.png" alt=""></a>
+      <a href="https://www.twitter.com/foodyfood_flori" target="nueva"><img class="footer-icon" src="images/tuiter.png" alt=""></a>
+    </div>
+  </footer>
+
+
   </body>
 </html>
+<?php } ?>
